2bc12dcb4ce45d656d6aac24dd7a5763d8bd7bb5
may-ank0813 <mayanksaini1913@gmail.com>
2022-03-11 21:55:07 +0530

    media_url

--- bike/urls.py
+++ bike/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views, utilitiesViews
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.static import serve
 
 urlpatterns = [
 	path('', views.home, name='home'),
@@ -20,6 +20,8 @@ urlpatterns = [
     
 	path('pdfView/<int:id>/', utilitiesViews.ViewPDF.as_view(), name="pdfView"),
     path('pdfDownload/<int:id>/', utilitiesViews.DownloadPDF.as_view(), name="pdfDownload"),
-
+    
+	re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
